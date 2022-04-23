4de0300c3d13aec511ae74abc58c7323f7d2d9d6
Lucho-1789 <89428281+Lucho-1789@users.noreply.github.com>
2022-04-23 12:57:30 -0300

    Primera subida
    Probando de nuevo el git

---
+++ js/operadores.js
@@ -0,0 +1,17 @@
+const mayoriaEdad = 18
+let edadUsuario1 = 15
+let edadUsuario2 = 25
+
+if(edadUsuario2 >= mayoriaEdad){
+    console.log("Puedes acceder a este contenido")
+}else{
+    console.log("No puedes acceder a este contenido")
+}
+
+// true / false
+let login = true
+if(!login = true){
+    console.log("Usuario admitido")
+}else{
+    console.log("Usuario no registrado")
+}
---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// Comentario simple
+/*
+comentario multilinea
+*/
+
+var saludo = "Hola mundo!";
+console.log(saludo)
+
+saludo = "Chau mundo!"
+console.log(saludo)
+
+let nombre = "Guido";
+nombre = "Maria";
+console.log(nombre);
+
+const pais = "Argentina";
+console.log(pais);
+document.write(pais);
+
+let cuenta = 10 + 20;
+console.log(cuenta);
+
+let boolean = true;
+let numeric = 250;
+
+console.log(document.URL);
+console.log(document);
+
+console.log(typeof(saludo))
+console.log(typeof(cuenta))
+console.log(typeof(boolean))
+
+
